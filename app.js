5bf8849fea1323f501ce5f140f408791b2508b1f
Arniss5 <kat.dobr@gmail.com>
2022-09-24 21:31:32 +0100

    add save btn functionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const incrementBtn = document.getElementById("increment");
 const decrementBtn = document.getElementById("decrement");
 const saveBtn = document.getElementById("save");
+//grab repeats
+const repeats = document.getElementById("repeats")
 //grab prev-repeats
 const prevRepeats = document.getElementById("prev-repeats");
 //initialise count to 0
@@ -10,19 +12,35 @@ let count = 0
 function increment() {
     //increment count by 1
     count++
-    console.log(count)
+    //get repeats element to display current count value
+    repeats.textContent = count
 }
 
 //create click event on increment button and assign increment function
 
 //create decrement function
 function decrement() {
-    //decrement count by 1
+    //decrement count by 1 unless count===0
     if (count > 0) {
         count--
     }
-    console.log(count)
+    //get repeats element to display current count value
+    repeats.textContent = count
 }
 
+//create click event on decrement button and assign decrement function
 
-//create click event on increment button and assign increment function
+//create save function 
+function save() {
+    //add current count value to prevRepeats textContent
+    prevRepeats.textContent += count + " | "
+    //reset count to 0
+    count = 0
+    //reset repeats textContent to 0
+    repeats.textContent = 0
+}
+
+//create click event on save button and assign save function
+
+
+
